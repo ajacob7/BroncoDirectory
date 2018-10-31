a8a588d110332e92845619e83c2c598e6d13ffdc
ajacob7 <39599106+ajacob7@users.noreply.github.com>
2018-10-31 12:43:03 -0700

    Delete databases.php

--- databases.php
+++
@@ -1,9 +0,0 @@
-
-
-<?php
-//This page will contain the login information to the scu server
-$username= "ajacob"; // SCUDC username
-$password="BroncoDirectory"; // SCUDC password
-$dbpath = "//dbserver.engr.scu.edu/db11g;" //
-
-?>
