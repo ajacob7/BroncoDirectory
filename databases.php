2f7285d1d91f50303249cbfb17829fd757ccc4a0
Asa <ajacob1@scu.edu>
2018-10-30 17:33:38 -0700

    Login info for php to server

---
+++ databases.php
@@ -0,0 +1,8 @@
+//This page will contain the login information to the scu server
+
+<?php
+$username= "XXXXXXXXXX"; // SCUDC username
+$password="XXXXXXXXXX"; // SCUDC password
+$dbpath = "//dbserver.engr.scu.edu/db11g;" //
+
+?>
