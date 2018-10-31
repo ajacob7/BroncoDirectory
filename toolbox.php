ecbfe7a81274bc097c6e775fef08c71b21e8b0b8
Asa <ajacob1@scu.edu>
2018-10-31 12:22:18 -0700

    viewQueue updated

--- toolbox.php
+++ toolbox.php
@@ -9,14 +9,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	 }
 }
 
-function connectToDB(){
-  $conn=oci_connect($username,$password, $dbpath);
-	if(!$conn) {
-	     print "<br> connection failed:";
-        exit;
-	}
-  return $conn;
-}
+// function connectToDB(){
+//   $conn=oci_connect($username,$password, $dbpath);
+// 	if(!$conn) {
+// 	     print "<br> connection failed:";
+//         exit;
+// 	}
+//   return $conn;
+// }
 function searchBar($name){
 	//connect to your database
 	$conn = connectToDB();
@@ -33,11 +33,58 @@ function searchBar($name){
 
 	return;
 }
+function getSalaryFromDB($name){
+	//connect to your database
+	$conn=oci_connect('ajacob','Craigtheclaw97', '//dbserver.engr.scu.edu/db11g');
+	if(!$conn) {
+	     print "<br> connection failed:";
+        exit;
+	}
+	//	 Parse the SQL query
+	$query = oci_parse($conn, "SELECT salary FROM AlphacoEmp where name= :bv");
 
+	oci_bind_by_name($query,':bv',$name);
+	// Execute the query
+	oci_execute($query);
+
+	if (($row = oci_fetch_array($query, OCI_BOTH)) != false) {
+		// We can use either numeric indexed starting at 0
+		// or the column name as an associative array index to access the colum value
+		// Use the uppercase column names for the associative array indices
+		echo $row[0] . " and " . $row['SALARY']   . " are the same<br>\n";
+		$salary = $row['SALARY'];
+	}
+	else {
+		echo "No such employee <br>\n";
+	}
+	oci_free_statement($query);
+	oci_close($conn);
+
+	return $salary;
+}
 function viewQueue(){
-  $conn = connectToDB();
+	$conn=oci_connect($username,$password, $dbpath);
+	if(!$conn) {
+			 print "<br> connection failed:";
+				exit;
+	}
+	//	 Parse the SQL query
+  $query = oci_parse($conn, 'SELECT businessName FROM Businesses INNER JOIN Verified ON verified.businessID = businesses.businessID WHERE VERIFIED = 1 ORDER BY CREATEDDATEANDTIME;')
+	// Execute the query
+	oci_execute($query);
 
-  $query = oci_parse($conn, 'SELECT businessName FROM Businesses INNER JOIN Verified ON verified.businessID = businesses.businessID WHERE VERIFIED = 1;')
+	// Prepare to display results
+	echo "<b>";
+	// Fetch each row. the first column is 0, then 1, etc.
+	while($row=oci_fetch_array($query)){
+		echo $row
+		}
+	}
+	echo "</b>";
+
+	oci_free_statement($query);
+	OCILogoff($conn);
+
 }
 
 function createListing(){
